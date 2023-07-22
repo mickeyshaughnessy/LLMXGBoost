ef6777dfdc02360472966c6543a2c47724051096
Mickey Shaughnessy <mickeyshaughnessy@gmail.com>
2023-07-22 10:22:53 -0600

    add train

---
+++ train.py
@@ -0,0 +1,13 @@
+# This script trains the LLM
+import xgboost as xgb
+
+
+if __name__ == "__main__":
+   # get the data
+   
+   # encode training data
+   # train model
+   # evaluate performance
+
+
+ 
